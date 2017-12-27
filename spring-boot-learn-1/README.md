9ddf494ccaeb8d5deb02e3c96bb9892a36e6c87e
csyeva <32060663+csy512889371@users.noreply.github.com>
2017-12-28 07:20:27 +0800

    spring-boot1

---
+++ spring-boot-learn-1/README.md
@@ -0,0 +1,8 @@
+
+# 搭建一个简单的RESTfull API接口项目
+
+
+
+## 启动项目
+直接运行main方法或者使用maven命令: spring-boot:run
+
---
+++ spring-boot-learn-1/src/main/java/com/eva/learn/bean/User.java
@@ -0,0 +1,37 @@
+package com.eva.learn.bean;
+
+import java.util.Date;
+
+/**
+ * 实体类
+ *
+ */
+public class User {
+    private int id;
+    private String name;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
